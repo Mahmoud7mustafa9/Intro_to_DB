791a340d2e1fb5159e4b93a1a8612f36aa6c4331
mahmoud moustafa <7odamostafa97@gmail.com>
2024-10-21 17:02:15 +0300

    ffv

---
+++ task_4.sql
@@ -0,0 +1,6 @@
+SELECT COLUMN_NAME , COLUMN_TYPE , IS_NULLABLE , COLUMN_KEY , COLUMN_DEFAULT , EXTRA
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = "alx_book_store"
+    TABLE_NAME = "Books"
